5a3098e9752bb1c4324c7d771b4724522bb13c38
yeonjoonhong <yeonjoonh@gmail.com>
2021-04-13 22:20:22 -0700

    Implemented Rectange class with GUI

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,26 @@
-public class Block {
+import java.awt.*;
+import javax.swing.*;
+
+public class Block extends JApplet{
+    Block(){
+        JFrame frame = new JFrame() {
+            @Override public void paint(Graphics g) {
+                g.drawRect(100, 100, 50, 100);
+            }
+        };
+
+        frame.setSize(300, 400);
+        frame.setTitle("Tetris Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Block m = new Block();
+
+    }
+
 
 }
